5aa6792967b4f2586bbac55ef70aa4a1d75a222d
Cade Parkhurst <parkhuca@rose-hulman.edu>
2020-04-07 00:47:14 -0400

    Added textToInt, and intToText for easier use

--- Tokenizer.py
+++ Tokenizer.py
@@ -19,3 +19,12 @@ token = tf.keras.preprocessing.text.Tokenizer(char_level=False, split=' ')
 token.fit_on_texts(getDictionary(df1))
 print(token.word_index)
 print(token.texts_to_sequences(["his with her"]))
+token.sequences_to_texts()
+
+
+def textToInt(input_text, tokenizer):
+    return tokenizer.text_to_sequences([input_text])
+
+
+def intToText(input_sequence, tokenizer):
+    return tokenizer.sequences_to_texts(input_sequence)
